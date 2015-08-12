022e3884756186071d87c8daa482e52e159b824d
unknown <lawalo@WITPB03BNBC.wit.private>
2015-08-12 16:56:22 -0400

    Added the drop down FAQ

--- page_festival.php
+++ page_festival.php
@@ -4,6 +4,8 @@
  */
 
 get_header(); ?>
+	<link href="/firstsite3/wp-content/themes/cityawake/style_cityawake.css" rel="stylesheet" />
+	<link href="/firstsite3/wp-content/themes/cityawake/festival.css" rel="stylesheet" />
 	<!-- ADDS FESTIVAL CLASS TO BODY -->
 	<script type="text/javascript">
 	$('body').addClass('festival');
@@ -20,6 +22,7 @@ get_header(); ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					
 					<div class="center-section">
 					<h2>About the Festival</h2>
 					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed sapien quam. Sed dapibus est id enim facilisis, at posuere turpis adipiscing. Quisque sit amet dui dui.</p>
@@ -35,14 +38,49 @@ get_header(); ?>
 						<p>Duis rhoncus velit nec est condimentum feugiat. Donec aliquam augue nec gravida lobortis. Nunc arcu mi, pretium quis dolor id, iaculis euismod ligula. Donec tincidunt gravida lacus eget lacinia.</p>
 						<div class="button" data-section="volunteer"></div>
 					</div><!-- .entry-content -->
-
+						<div id="faq_block">
+        					<p id="header">FAQ</p>
+      						<div class="faq_container">
+        						<div class = "faq">
+            						<div id="question">What is an FAQ?</div>
+            						<br>
+            						<div id="answer">IDK Leave me alone</div>
+        						</div>
+        
+        						<div class = "faq">
+            						<div id="question">What is an FAQ?</div>
+            						<br>
+            						<div id="answer">IDK Leave me alone</div>
+        						</div>
+        
+        						<div class = "faq">
+            						<div id="question">What is an FAQ?</div>
+           	 						<br>
+            						<div id="answer">IDK Leave me alone</div>
+        						</div>
+        	
+        						<div class = "faq">
+            						<div id="question">What is an FAQ?</div>
+            						<br>
+            						<div id="answer">IDK Leave me alone</div>
+        						</div> 
+        
+        						<div class = "faq">
+            						<div id="question">What is an FAQ?</div>
+            						<br>
+            						<div id="answer">IDK Leave me alone</div>
+        						</div>
+        
+        						<div class = "faq">
+            						<div id="question">What is an FAQ?</div>
+            						<br>
+            						<div id="answer">IDK Leave me alone</div>
+        						</div> 
+      						</div>
+    					</div>
 					<div class="center-section">
-					<h2>FAQs</h2>
-						<div class="faq">
-							<div class="question">Pizza</div>
-							<div class="answer">Pizaaaza</div>
-						</div>
 
+					</div>
 
 
 				</article><!-- #post -->
