8f455c55488c83a8690aef773ba91cdab1b975c9
unknown <lawalo@WITPB03BNBC.wit.private>
2015-08-14 14:40:46 -0400

    Fix the FAQ to look more like the wireframe also addded some javascript for the faq drop down

--- page_festival.php
+++ page_festival.php
@@ -6,6 +6,9 @@
 get_header(); ?>
 	<link href="/firstsite3/wp-content/themes/cityawake/style_cityawake.css" rel="stylesheet" />
 	<link href="/firstsite3/wp-content/themes/cityawake/festival.css" rel="stylesheet" />
+	<script src="https://code.jquery.com/jquery-3.0.0-alpha1.js"></script>
+	<script src="https://code.jquery.com/jquery-2.1.4.js"></script>
+	
 	<!-- ADDS FESTIVAL CLASS TO BODY -->
 	<script type="text/javascript">
 	$('body').addClass('festival');
@@ -13,6 +16,16 @@ get_header(); ?>
 	$('.faq').click(function(){
 		// $(this).children('.answer').toggleClass('open');
 		alert("CLICKED!!");
+
+	$(".faq").click(function(){
+    	$(this).css("padding-bottom", "200px");
+    	$(this).css("background-color", "rgb(225, 0, 0)");
+	});
+
+	$(".faq").mouseleave(function(){
+    	$(this).css("padding-bottom", "30px"); 
+    	$(this).css("background-color", "rgb(255, 60, 50)");
+	});
 	})
 	</script>
 	<div id="primary" class="content-area festival">
@@ -38,46 +51,46 @@ get_header(); ?>
 						<p>Duis rhoncus velit nec est condimentum feugiat. Donec aliquam augue nec gravida lobortis. Nunc arcu mi, pretium quis dolor id, iaculis euismod ligula. Donec tincidunt gravida lacus eget lacinia.</p>
 						<div class="button" data-section="volunteer"></div>
 					</div><!-- .entry-content -->
-						<div id="faq_block">
-        					<p id="header">FAQ</p>
-      						<div class="faq_container">
-        						<div class = "faq">
-            						<div id="question">What is an FAQ?</div>
-            						<br>
-            						<div id="answer">IDK Leave me alone</div>
-        						</div>
+						 <div id="faq_block">
+        <p id="header">FAQ</p>
+      <div class="faq_container">
+        <div class = "faq">
+            <div id="question">Ut posuere elit non convalis bladit ut posuere elit non?</div>
+            <div id="answer">IDK Leave me alone</div>
+            <div class="arrow right"></div>
+        </div>
         
-        						<div class = "faq">
-            						<div id="question">What is an FAQ?</div>
-            						<br>
-            						<div id="answer">IDK Leave me alone</div>
-        						</div>
+        <div class = "faq">
+            <div id="question">Ut posuere elit non convalis bladit ut posuere elit non?</div>
+            <div id="answer">IDK Leave me alone</div>
+            <div class="arrow right"></div>
+        </div>
         
-        						<div class = "faq">
-            						<div id="question">What is an FAQ?</div>
-           	 						<br>
-            						<div id="answer">IDK Leave me alone</div>
-        						</div>
-        	
-        						<div class = "faq">
-            						<div id="question">What is an FAQ?</div>
-            						<br>
-            						<div id="answer">IDK Leave me alone</div>
-        						</div> 
+        <div class = "faq">
+            <div id="question">Ut posuere elit non convalis bladit ut posuere elit non?</div>
+            <div id="answer">IDK Leave me alone</div>
+            <div class="arrow right"></div>
+        </div>
         
-        						<div class = "faq">
-            						<div id="question">What is an FAQ?</div>
-            						<br>
-            						<div id="answer">IDK Leave me alone</div>
-        						</div>
+        <div class = "faq">
+            <div id="question">Ut posuere elit non convalis bladit ut posuere elit non?</div>
+            <div id="answer">IDK Leave me alone</div>
+            <div class="arrow right"></div>
+        </div> 
         
-        						<div class = "faq">
-            						<div id="question">What is an FAQ?</div>
-            						<br>
-            						<div id="answer">IDK Leave me alone</div>
-        						</div> 
-      						</div>
-    					</div>
+        <div class = "faq">
+            <div id="question">Ut posuere elit non convalis bladit ut posuere elit non?</div>
+            <div id="answer">IDK Leave me alone</div>
+            <div class="arrow right"></div>
+        </div>
+        
+        <div class = "faq">
+            <div id="question">Ut posuere elit non convalis bladit ut posuere elit non?</div>
+            <div id="answer">IDK Leave me alone</div>
+            <div class="arrow right"></div>
+        </div> 
+      </div>
+    </div>
 					<div class="center-section">
 
 					</div>
